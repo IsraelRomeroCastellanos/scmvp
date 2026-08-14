06b635f96e87aaa77f9ccd410205ae143aeadf7b
Israel <israelrc@ntegix.com>
2026-08-14 10:29:50 -0600

    fix(migrations): use stable index predicate serialization in 003

--- backend/migrations/20260805_003_gestion_matrices_empresa.verify.sql
+++ backend/migrations/20260805_003_gestion_matrices_empresa.verify.sql
@@ -250,7 +250,7 @@ BEGIN
       pg_catalog.array_agg(a.attname::TEXT ORDER BY k.ord) FILTER (WHERE k.ord<=i.indnkeyatts) AS columnas,
       pg_catalog.bool_or(k.attnum=0) FILTER (WHERE k.ord<=i.indnkeyatts) AS tiene_expresiones,
       pg_catalog.lower(pg_catalog.regexp_replace(
-        pg_catalog.pg_get_expr(i.indpred,i.indrelid,true),'[[:space:]]+','','g')) AS predicado
+        pg_catalog.pg_get_expr(i.indpred,i.indrelid,false),'[[:space:]]+','','g')) AS predicado
       INTO real
       FROM pg_catalog.pg_namespace n JOIN pg_catalog.pg_class t ON t.relnamespace=n.oid
       JOIN pg_catalog.pg_index i ON i.indrelid=t.oid
